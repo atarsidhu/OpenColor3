429b09c7c9559e9648e6396d44f7adbf998d7910
Andrew Ledger <andrew.ledger@student.ufv.ca>
2020-05-20 09:10:02 -0700

    App Asks for camera permission

--- app/src/main/java/com/example/opencolor/Picture.java
+++ app/src/main/java/com/example/opencolor/Picture.java
@@ -1,6 +1,9 @@
 package com.example.opencolor;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
@@ -8,6 +11,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import android.os.Bundle;
 
 public class Picture extends AppCompatActivity {
@@ -17,11 +23,14 @@ public class Picture extends AppCompatActivity {
     ImageView imageView;
     private static final int PICTURE_ID = 1;
     private static final int GALLERY_REQUEST_CODE = 2;
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 10;
+    public static Activity pictureActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
+        pictureActivity = this;
 
         imageView = findViewById(R.id.imageView);
 
@@ -31,7 +40,15 @@ public class Picture extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent startIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(startIntent, PICTURE_ID);
+                if (startIntent.resolveActivity(getPackageManager()) != null) {
+                    if (ContextCompat.checkSelfPermission(pictureActivity, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                        startActivityForResult(startIntent, PICTURE_ID);
+                    }
+                    else{
+                        ActivityCompat.requestPermissions(pictureActivity, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+                        startActivityForResult(startIntent, PICTURE_ID);
+                    }
+                }
             }
         });
 
@@ -50,8 +67,9 @@ public class Picture extends AppCompatActivity {
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // Taking picture from camera
-        if (requestCode == PICTURE_ID) {
-            Bitmap photo = (Bitmap)data.getExtras().get("data");
+        if (requestCode == PICTURE_ID && resultCode == RESULT_OK && data != null) {
+            super.onActivityResult(requestCode, resultCode, data);
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(photo);
         }
 
