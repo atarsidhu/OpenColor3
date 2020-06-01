8d3f761d7d0d24defc3ecbe1cc5484428bfbc0ec
Andrew Ledger <andrew.ledger@student.ufv.ca>
2020-05-31 22:28:51 -0700

    Added "Return Home" Button

--- app/src/main/java/com/example/opencolor/Results.java
+++ app/src/main/java/com/example/opencolor/Results.java
@@ -1,5 +1,9 @@
 package com.example.opencolor;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +11,7 @@ import android.os.Bundle;
 public class Results extends AppCompatActivity {
 
     TextView tvResults;
+    Button homeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +19,17 @@ public class Results extends AppCompatActivity {
         setContentView(R.layout.activity_results);
 
         tvResults = findViewById(R.id.tvResults);
+        homeButton = findViewById(R.id.homeButton);
 
         tvResults.setText("(DEV PURPOSES) Based on the results, you have ______ \n" +
                 "Results: " + getIntent().getStringExtra("RESULTS"));
 
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
